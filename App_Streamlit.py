08756481d64b06aa1d2bed43df8b40cb7d8933b5
saumyagajjar <83940362+saumyagajjar@users.noreply.github.com>
2023-04-24 18:26:47 +0530

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -222,7 +222,7 @@ def main():
     st.sidebar.text("Built with Streamlit")
     
     st.sidebar.header("For Any Queries/Suggestions Please reach out at :")
-    st.sidebar.info("saumyagajjar2409@gmail.com")
+    st.sidebar.info("saumyagajjar9898763321@gmail.com")
     #st.sidebar.subheader("Scatter-plot setup")
     #box1 = st.sidebar.selectbox(label= "X axis", options = numeric_columns)
     #box2 = st.sidebar.selectbox(label="Y axis", options=numeric_columns)
